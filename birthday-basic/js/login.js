9ba205e4ec1af02a154e7cdd628d394129aa7cb5
moli531 <82210228+moli531@users.noreply.github.com>
2021-04-09 18:28:03 +0800

    Update login.js

--- birthday-basic/js/login.js
+++ birthday-basic/js/login.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="姨父" &&  pwd=="happy birthday!"){
+    if(userName=="生日快乐" &&  pwd=="happy birthday"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
